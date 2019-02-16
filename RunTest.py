bdf3860b1642d0ec823c264b14460da36eb7eeb3
ehmatthes <wangyizhi954@gmail.com>
2019-02-16 15:03:22 +0800

    添加说明

--- RunTest.py
+++ RunTest.py
@@ -1,4 +1,4 @@
 #encoding=utf-8
 from chapt_15.DataDrivenFrameWork.testScripts.TestMail126AddContacts import *
 if __name__ == '__main__':
-    test126MailAddContacts()
+    test126MailAddContacts()  #执行测试
